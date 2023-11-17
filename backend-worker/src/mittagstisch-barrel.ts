9c68abc8d00c0f1e4e221a48d350d78b8a0a628d
Felix Wagner <fwagner@mailbox.org>
2023-11-17 18:41:13 +0100

    refactor: cleanup unused params

--- backend-worker/src/mittagstisch-barrel.ts
+++ backend-worker/src/mittagstisch-barrel.ts
@@ -1,14 +1,15 @@
 import { Lunch } from './models/lunch.interface';
+import { Restaurant } from './models/restaurant.interface';
 import bicLunch from './restaurants/bistro-bic';
 
-const restaurants: [Restaurant] = [
+const restaurants: Restaurant[] = [
 	{
 		getLunch: bicLunch,
 	},
 ];
 
 export default {
-	async fetch(dayOfWeek: number, request: Request, env: Env, ctx: ExecutionContext): Promise<Lunch[]> {
+	async fetch(dayOfWeek: number): Promise<Lunch[]> {
 		const lunches = await Promise.all(restaurants.map((res) => res.getLunch(dayOfWeek)));
 
 		return lunches.filter((l) => !!l).map((l) => <Lunch>l);
